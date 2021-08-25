156e4d8435473e5748854a39097e0b7baaad044b
arr19422 <arr19422@uvg.edu.gt>
2021-08-24 20:34:47 -0600

    Refactor y optimizacion

--- SR4.py
+++ SR4.py
@@ -2,6 +2,6 @@ import gl as gl
 
 
 render = gl.Renderer(1000, 1000)
-render.load('./models/cube2.obj', [6 , 5, 0], [100, 100, 120])
+render.load('./models/baymax.obj', [50, 1, 0], [10, 10, 3])
 render.glFinish('./x.bmp')
 render.glFinishZ('./x2.bmp')
